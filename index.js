4d7e54d8b5863628399d25d1d3adac5c72ab71a1
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-17 17:57:31 -0400

    viewDepartments function working

--- index.js
+++ index.js
@@ -3,19 +3,53 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 // TODO: connect to sql database
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: 'Hakurayn11!',
+    database: 'employee_db',
+  });
 
 // TODO: inquirer prompts
-function name() {
+function start() {
     inquirer.prompt([
         {
             type: "list",
-            name: firstPrompt,
+            name: "firstPrompt",
             message: "What would you like to do?",
-            choices: ["View departments", "View roles", "View employees", "Add departments", "Add roles", "Add employees", "Update departments", "Update roles", "Update employees"]
+            choices: ["View departments", "View roles", "View employees", "Add departments", "Add roles", "Add employees", "Update departments", "Update roles", "Update employees", "Exit"]
         }
-    ])
+    ]).then ((response) => {
+        switch (response.firstPrompt) {
+            case "View departments":
+                viewDepartments();
+                break;
 
+            default:
+                break;
+        }
+    })
 }
+
+// Prompt functions
+function viewDepartments() {
+    connection.query(
+        "SELECT * FROM department;",
+        (err, results) => {
+            if (err) throw err;
+            console.table(results);
+            start();
+        }
+    )
+};
+
 // add departments, roles, employees
 // view departments, roles, employees
 // update employee roles
@@ -23,6 +57,11 @@ function name() {
 // TODO: post employee data to sql database
 
 // TODO: connect to server and database
+connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
 
 // TODO: ??? SQL JOINS ???
 // TODO: SQL queries/constructor functions/classes

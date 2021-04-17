555f22e3f4f9c8786a63aab87711f2c91c87cc36
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-17 16:49:18 -0400

    installed console.table package

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const mysql = require("mysql");
 
 // TODO: connect to sql database
 
-// TODO: user prompts function using inquirer
+// TODO: inquirer prompts
+// add departments, roles, employees
+// view departments, roles, employees
+// update employee roles
 
 // TODO: post employee data to sql database
-
-// TODO: delete employee data from sql database
 
 // TODO: connect to server and database

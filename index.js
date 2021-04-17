25bf573310d316b215478ef5e4e0621d5b66b500
Anisha McDowell <anishamcdowell@gmail.com>
2021-04-17 16:32:24 -0400

    index TODOs established

---
+++ index.js
@@ -0,0 +1,13 @@
+//package dependencies
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+// TODO: connect to sql database
+
+// TODO: user prompts function using inquirer
+
+// TODO: post employee data to sql database
+
+// TODO: delete employee data from sql database
+
+// TODO: connect to server and database
